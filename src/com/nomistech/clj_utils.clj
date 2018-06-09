7b39c79ebb76997cd23f3c407be2215f6e9ba106
Simon Katz <sk@nomistech.com>
2018-06-09 17:51:05 +0100

    Move stuff to here from `com.nomistech/clojure-the-language`

--- src/com/nomistech/clj_utils.clj
+++ src/com/nomistech/clj_utils.clj
@@ -1,1 +1,257 @@
 (ns com.nomistech.clj-utils)
+
+;;;; ___________________________________________________________________________
+;;;; ---- do1 ----
+
+(defmacro do1
+  "Evaluates all the forms and returns the result of the first form."
+  {:style/indent 1}
+  [form-1 & other-forms]
+  `(let [result# ~form-1]
+     ~@other-forms
+     result#))
+
+;;;; ___________________________________________________________________________
+;;;; ---- do2 ----
+
+(defmacro do2
+  "Evaluates all the forms and returns the result of the second form."
+  {:style/indent 2}
+  [form-1 form-2 & other-forms]
+  `(do
+     ~form-1
+     (do1
+         ~form-2
+       ~@other-forms)))
+
+;;;; ___________________________________________________________________________
+;;;; ---- econd ----
+
+(defmacro econd
+  "Like `cond`, except throws a RuntimeException if no clause matches."
+  [& clauses]
+  `(cond ~@clauses
+         :else (throw (RuntimeException. "econd has no matching clause"))))
+
+;;;; ___________________________________________________________________________
+;;;; Maybe use the following approach instead of `map-keys` and `map-vals`
+
+#_
+(->> m
+     (map (fn [[k v]] [k (f1 v)]))
+     (map (fn [[k v]] [(f2 k) v]))
+     (map (fn [[k v]] [(f3 k) (f4 v)]))
+     (into {}))
+
+;;;; ___________________________________________________________________________
+;;;; ---- map-keys ----
+
+(defn map-keys [f m]
+  (into {}
+        (for [[k v] m]
+          [(f k) v])))
+
+;;;; ___________________________________________________________________________
+;;;; ---- map-vals ----
+
+(defn map-vals [f m]
+  (into {}
+        (for [[k v] m]
+          [k (f v)])))
+
+;;;; ___________________________________________________________________________
+;;;; ---- transitive-closure ----
+
+(defn ^:private transitive-closure-helper [f visited sofar vs]
+  (letfn [(helper [visited sofar vs]
+            (if (empty? vs)
+              sofar
+              (let [next-vs (set (->> (mapcat f vs)
+                                      (remove visited)
+                                      (remove vs)))]
+                (recur (into visited next-vs)
+                       (into sofar next-vs)
+                       next-vs))))]
+    (helper visited sofar vs)))
+
+(defn transitive-closure
+  "The set of values obtained by starting with v, then applying f to v,
+  then applying f to each of the results, and so on. v and all
+  intermediate values are included in the result."
+  [f v]
+  (transitive-closure-helper f #{v} #{v} #{v}))
+
+;;;; ___________________________________________________________________________
+;;;; ---- transitive-closure-excluding-self ----
+
+(defn transitive-closure-excluding-self
+  "The set of values obtained by applying f to v,
+  then applying f to each of the results, and so on. All
+  intermediate values are included in the result."
+  [f v]
+  (transitive-closure-helper f #{v} #{} #{v}))
+
+;;;; ___________________________________________________________________________
+;;;; ---- invert-function invert-relation ----
+
+(defn invert-function [f domain-subset]
+  "Return a map that represents the inverse of `f`.
+  `f` takes elements of `domain-subset` (and possibly other values, not
+  relevant here) as argument, and returns a single value.
+  For explanations of terminology, see:
+    https://www.mathsisfun.com/sets/domain-range-codomain.html"
+  (dissoc (group-by f domain-subset)
+          nil))
+
+(defn invert-relation [rel domain-subset]
+  "Return a map which represents the inverse of `rel`.
+  `rel` takes elements of `domain-subset` (and possibly other values, not
+  relevant here) as argument, and returns a collection of values.
+  For explanations of terminology, see:
+    https://www.mathsisfun.com/sets/domain-range-codomain.html"
+  (let [domain-range-pairs (for [d domain-subset
+                                 r (rel d)]
+                             [d r])]
+    (reduce (fn [sofar [d r]]
+              (update sofar
+                      r
+                      (fnil conj [])
+                      d))
+            {}
+            domain-range-pairs)))
+
+;;;; ___________________________________________________________________________
+;;;; ---- with-extras ----
+
+(defmacro with-extras [[& {:keys [before after]}]
+                       & body]
+  "Does `before`, then `body`, then `after`. Returns the result of `body`.
+  If `body` throws an exception, `after` is still done."
+  `(do ~before
+       (try (do ~@body)  
+            (finally 
+              ~after))))
+
+;;;; ___________________________________________________________________________
+;;;; ---- member? ----
+
+(defn member? [item coll]
+  (some #{item} coll))
+
+;;;; ___________________________________________________________________________
+;;;; ---- submap? ----
+
+(defn submap? [m1 m2]
+  (= m1 (select-keys m2 (keys m1))))
+
+(defn submap?-v2 [m1 m2]
+  (clojure.set/subset? (set m1) (set m2)))
+
+;;;; ___________________________________________________________________________
+;;;; ---- deep-merge ----
+
+(defn deep-merge
+  "Recursively merges maps. If vals are not maps, the last value wins."
+  [& vals]
+  (if (every? map? vals)
+    (apply merge-with deep-merge vals)
+    (last vals)))
+
+;;;; ___________________________________________________________________________
+;;;; ---- select-keys-recursively ----
+
+(defn select-keys-recursively
+  "Similar to `select-keys`, but with key paths digging in to a nested map.
+  `key-paths` is a sequence. Each element of `key-paths` begins with a key, k,
+  to be selected and is followed by a vector of key paths that specify the
+  things to be selected from the value corresponding to k.
+  Example:
+      (select-keys-recursively {:k-1 \"v-1\"
+                                :k-2 {:k-2-1 \"v-2-1\"
+                                      :k-2-2 {:k-2-2-1 \"v-2-2-1\"
+                                              :k-2-2-2 \"v-2-2-2\"
+                                              :k-2-2-3 \"v-2-2-3\"}}
+                                :k-3 \"v-3\"}
+                               [[:k-1]
+                                [:k-2 [:k-2-2
+                                        [:k-2-2-1]
+                                        [:k-2-2-3]]]])
+      => {:k-1 \"v-1\"
+          :k-2 {:k-2-2 {:k-2-2-1 \"v-2-2-1\"
+                        :k-2-2-3 \"v-2-2-3\"}}}"
+  [m key-paths]
+  (or (apply merge
+             (for [p key-paths]
+               (let [n (count p)]
+                 (case n
+                   0 (throw (Error. "Empty path in key-paths"))
+                   1 (select-keys m [(first p)])
+                   (if-not (contains? m (first p))
+                     {}
+                     {(first p) (select-keys-recursively (get m (first p))
+                                                         (rest p))})))))
+      {}))
+
+;;;; ___________________________________________________________________________
+;;;; ---- indexed ----
+
+(defn indexed
+  ;; From http://stackoverflow.com/questions/4830900, with changes.
+  "Returns a lazy sequence of [index, item] pairs, where items come
+  from 's' and indexes count up from zero.
+  eg:
+      (indexed '(a b c d))  =>  ([0 a] [1 b] [2 c] [3 d])"
+  [s]
+  (map-indexed vector s))
+
+;;;; ___________________________________________________________________________
+;;;; ---- position ----
+;;;; ---- positions ----
+
+(defn positions
+  ;; From http://stackoverflow.com/questions/4830900.
+  "Returns a lazy sequence containing the positions at which pred
+  is true for items in coll."
+  [pred coll]
+  (for [[idx elt] (indexed coll)
+        :when (pred elt)]
+    idx))
+
+(defn position
+  [pred coll]
+  (first (positions pred coll)))
+
+;;;; ___________________________________________________________________________
+;;;; ---- unchunk ----
+
+(defn unchunk
+  "Produce a fully-lazy sequence from `s`.
+  Sometimes Clojure sequences are not fully lazy.
+  See http://blog.fogus.me/2010/01/22/de-chunkifying-sequences-in-clojure/
+  for details.
+  See also Stuart Sierra comment at https://stackoverflow.com/questions/3407876/how-do-i-avoid-clojures-chunking-behavior-for-lazy-seqs-that-i-want-to-short-ci"
+  [s]
+  ;; TODO Is there a reason to prefer one of these over the other?
+  ;;      - Isn't `rest` lazier than `next`?
+  ;;        - Add auto-test that would distinguish. (Or do you already have one?
+  (case 2
+    1 (when (seq s)
+        (lazy-seq
+         (cons (first s)
+               (unchunk (next s)))))
+    2 (lazy-seq
+       (when-let [[x] (seq s)]
+         (cons x (unchunk (rest s)))))))
+
+;;;; ___________________________________________________________________________
+;;;; ---- last-index-of-char-in-string ----
+
+(defn last-index-of-char-in-string [^Character char ^String string]
+  ;; Effect of type hints:
+  ;;   Without:
+  ;;     (time (dotimes [i 1000000] (last-index-of-char-in-string \c "abcdef")))
+  ;;     "Elapsed time: 2564.688 msecs"
+  ;;   With:
+  ;;     (time (dotimes [i 1000000] (last-index-of-char-in-string \c "abcdef")))
+  ;;     "Elapsed time: 18.44 msecs"
+  (.lastIndexOf string (int char)))
--- test/com/nomistech/clj_utils_test.clj
+++ test/com/nomistech/clj_utils_test.clj
@@ -1,3 +1,436 @@
 (ns com.nomistech.clj-utils-test
   (:require [com.nomistech.clj-utils :as sut]
             [midje.sweet :refer :all]))
+
+;;;; ___________________________________________________________________________
+;;;; ---- sut/do1 ----
+
+(fact "`sut/do1` works"
+
+  (fact "Fails to compile when there are no forms"
+    (macroexpand-1 '(sut/do1))
+    => (throws clojure.lang.ArityException))
+  
+  (fact "Returns value of first form when there is one form"
+    (sut/do1 :a)
+    => :a)
+  
+  (fact "Returns value of first form when there are two forms"
+    (sut/do1
+        :a
+      :b)
+    => :a)
+  
+  (fact "Returns value of first form when there are three forms"
+    (sut/do1
+        :a
+      :b
+      :c)
+    => :a)
+
+  (fact "Forms are evaluated in correct order"
+    (let [side-effect-place (atom [])]
+      (sut/do1
+          (swap! side-effect-place conj 1)
+        (swap! side-effect-place conj 2)
+        (swap! side-effect-place conj 3))
+      => anything
+      (fact 
+        @side-effect-place => [1 2 3]))))
+
+;;;; ___________________________________________________________________________
+;;;; ---- sut/do2 ----
+
+(fact "`sut/do2` works"
+
+  (fact "Fails to compile when there are no forms"
+    (macroexpand-1 '(sut/do2))
+    => (throws clojure.lang.ArityException))
+
+  (fact "Fails to compile when there is one forms"
+    (macroexpand-1 '(sut/do2 :a))
+    => (throws clojure.lang.ArityException))
+  
+  (fact "Returns value of second form when there are two forms"
+    (sut/do2
+        :a
+        :b)
+    => :b)
+  
+  (fact "Returns value of second form when there are three forms"
+    (sut/do2
+        :a
+        :b
+      :c)
+    => :b)
+
+  (fact "Forms are evaluated in correct order"
+    (let [side-effect-place (atom [])]
+      (sut/do2
+          (swap! side-effect-place conj 1)
+          (swap! side-effect-place conj 2)
+        (swap! side-effect-place conj 3))
+      => anything
+      (fact 
+        @side-effect-place => [1 2 3]))))
+
+;;;; ___________________________________________________________________________
+;;;; ---- sut/econd ----
+
+(fact "`sut/econd` works"
+  (fact "no clauses"
+    (sut/econd)
+    => (throws RuntimeException))
+  (fact "many clauses"
+    (fact "last clause truthy"
+      (sut/econd false 1
+                 nil   2
+                 :this-one 3)
+      => 3)
+    (fact "non-last clause truthy"
+      (sut/econd false 1
+                 nil   2
+                 :this-one 3
+                 :not-this-one 4)
+      => 3)
+    (fact "none truthy"
+      (sut/econd false 1
+                 nil   2
+                 false 3
+                 nil   4)
+      => (throws RuntimeException))))
+
+;;;; ___________________________________________________________________________
+;;;; ---- sut/map-keys ----
+
+(fact "`sut/map-keys`" works
+  (sut/map-keys keyword
+                {"a" 1
+                 "b" 2})
+  => {:a 1
+      :b 2})
+
+;;;; ___________________________________________________________________________
+;;;; ---- sut/map-vals ----
+
+(fact "`sut/map-vals`" works
+  (sut/map-vals inc
+                {:a 1
+                 :b 2})
+  => {:a 2
+      :b 3})
+
+;;;; ___________________________________________________________________________
+;;;; ---- sut/transitive-closure ----
+
+;; TODO: More substantial tests of `sut/transitive-closure`, e.g. edge cases.
+
+(fact "`sut/transitive-closure` works"
+  (sut/transitive-closure (fn [x]
+                            (case x
+                              1 [1 2 3 4]
+                              2 [200 1]
+                              3 [300 1]
+                              4 [1]
+                              []))
+                          1)
+  => #{1 2 3 4 200 300})
+
+;;;; ___________________________________________________________________________
+;;;; ---- sut/transitive-closure-excluding-self ----
+
+;; TODO: More substantial tests of `sut/transitive-closure-excluding-self`,
+;;       e.g. edge cases.
+
+(fact "`sut/transitive-closure-excluding-self` works"
+  (sut/transitive-closure-excluding-self (fn [x]
+                                           (case x
+                                             1 [1 2 3 4]
+                                             2 [200 1]
+                                             3 [300 1]
+                                             4 [1]
+                                             []))
+                                         1)
+  => #{2 3 4 200 300})
+
+;;;; ___________________________________________________________________________
+;;;; ---- sut/invert-function sut/invert-relation ----
+
+(fact "`sut/invert-function` works"
+  (sut/invert-function {:a 1
+                        :b 2
+                        :c 3
+                        :d 1}
+                       [:a :b :c :d :e])
+  =>
+  {1 [:a :d]
+   2 [:b]
+   3 [:c]})
+
+(fact "`sut/invert-relation` works"
+  (sut/invert-relation {:a [1 2]
+                        :b [2 3]
+                        :c []
+                        :d [2]}
+                       [:a :b :c :d :e])
+  =>
+  {1 [:a]
+   2 [:a :b :d]
+   3 [:b]})
+
+;;;; ___________________________________________________________________________
+;;;; ---- sut/with-extras ----
+
+(fact "`sut/with-extras` works"
+
+  (fact "without an exception"
+    (let [side-effect-place (atom [])]
+      (fact "Value is correct"
+        (sut/with-extras [:before (swap! side-effect-place conj 1)
+                          :after  (swap! side-effect-place conj 3)] 
+          (do (swap! side-effect-place conj 2)
+              :a))
+        => :a)
+      (fact "Forms are evaluated in correct order"
+        @side-effect-place => [1 2 3])))
+
+  (fact "with an exception"
+    (let [side-effect-place (atom [])]
+      (fact "throws"
+        (sut/with-extras [:before (swap! side-effect-place conj 1)
+                          :after  (swap! side-effect-place conj 3)] 
+          (do (/ 0 0)
+              (swap! side-effect-place conj 2)
+              :a))
+        => throws)
+      (fact "`after` is still done"
+        @side-effect-place => [1 3]))))
+
+;;;; ___________________________________________________________________________
+;;;; ---- sut/member? ----
+
+(fact "`sut/member?` works"
+  (fact "Returns truthy if the item is in the collection"
+    (sut/member? :b [:a :b :c]) => truthy)
+  (fact "Returns falsey if the item is not in the collection"
+    (sut/member? :d []) => falsey
+    (sut/member? :d [:a :b :c]) => falsey))
+
+;;;; ___________________________________________________________________________
+;;;; ---- sut/submap? ----
+
+(fact "`sut/submap?` works"
+  (do
+    (fact (sut/submap? {}     {}) => true)
+    (fact (sut/submap? {:a 1} {}) => false))
+  (do
+    (fact (sut/submap? {}               {:a 1 :b 2}) => true)
+    (fact (sut/submap? {:a 1}           {:a 1 :b 2}) => true)
+    (fact (sut/submap? {:a 1 :b 2}      {:a 1 :b 2}) => true))
+  (do
+    (fact (sut/submap? {:a 1 :b 2 :c 3} {:a 1 :b 2}) => false)
+    (fact (sut/submap? {:a 9}           {:a 1 :b 2}) => false)
+    (fact (sut/submap? {:a 9 :b 2}      {:a 1 :b 2}) => false)))
+
+(fact "`sut/submap?-v2` works
+  (do
+    (fact (sut/submap?-v2 {}     {}) => true)
+    (fact (sut/submap?-v2 {:a 1} {}) => false))"
+  (do
+    (fact (sut/submap?-v2 {}               {:a 1 :b 2}) => true)
+    (fact (sut/submap?-v2 {:a 1}           {:a 1 :b 2}) => true)
+    (fact (sut/submap?-v2 {:a 1 :b 2}      {:a 1 :b 2}) => true))
+  (do
+    (fact (sut/submap?-v2 {:a 1 :b 2 :c 3} {:a 1 :b 2}) => false)
+    (fact (sut/submap?-v2 {:a 9}           {:a 1 :b 2}) => false)
+    (fact (sut/submap?-v2 {:a 9 :b 2}      {:a 1 :b 2}) => false)))
+
+;;;; ___________________________________________________________________________
+;;;; ---- sut/deep-merge ----
+
+(fact "`sut/deep-merge` works"
+
+  (fact "non-conflicting merge"
+    (sut/deep-merge {:a 1
+                     :b 2}
+                    {:c 3})
+    => {:a 1
+        :b 2
+        :c 3})
+  
+  (fact "replacing merge"
+    (sut/deep-merge {:a 1
+                     :b {:bb 22}}
+                    {:b 999})
+    => {:a 1
+        :b 999})
+  
+  (fact "deep merge"
+    (sut/deep-merge {:a 1
+                     :b {:bb 22}}
+                    {:b {:ba 21
+                         :bb 999}})
+    => {:a 1
+        :b {:ba 21
+            :bb 999}})
+  
+  (fact "merge in an empty map"
+    (sut/deep-merge {:a 1 :b {:bb 22}}
+                    {:b {}})
+    => {:a 1 :b {:bb 22}})
+  
+  (fact "merge in nil"
+    (sut/deep-merge {:a 1 :b {:bb 22}}
+                    {:b nil})
+    => {:a 1 :b nil})
+  
+  (fact "merge multiple maps"
+    (sut/deep-merge {:a 1 :b 2 :c 3}
+                    {:a 11 :b 12}
+                    {:a 101})
+    => {:a 101 :b 12 :c 3}))
+
+;;;; ___________________________________________________________________________
+;;;; ---- sut/select-keys-recursively ----
+
+(fact "`sut/select-keys-recursively` works"
+
+  (let [m {:k-1 "v-1"
+           :k-2 {:k-2-1 "v-2-1"
+                 :k-2-2 {:k-2-2-1 "v-2-2-1"
+                         :k-2-2-2 "v-2-2-2"
+                         :k-2-2-3 "v-2-2-3"}}
+           :k-3 "v-3"}]
+
+    (fact
+      (sut/select-keys-recursively m [])
+      => {})
+
+    (fact
+      (sut/select-keys-recursively m [[]])
+      => (throws))
+
+    (fact
+      (sut/select-keys-recursively m [[:no-such-key]])
+      => {})
+
+    (fact
+      (sut/select-keys-recursively m [[:k-1]])
+      => {:k-1 "v-1"})
+
+    (fact
+      (sut/select-keys-recursively m [[:k-1]
+                                      [:k-2]])
+      => {:k-1 "v-1"
+          :k-2 {:k-2-1 "v-2-1"
+                :k-2-2 {:k-2-2-1 "v-2-2-1"
+                        :k-2-2-2 "v-2-2-2"
+                        :k-2-2-3 "v-2-2-3"}}})
+
+    (fact
+      (sut/select-keys-recursively m [[:k-1]
+                                      [:k-2 [:k-2-2
+                                             [:k-2-2-1]
+                                             [:k-2-2-3]]]])
+      => {:k-1 "v-1"
+          :k-2 {:k-2-2 {:k-2-2-1 "v-2-2-1"
+                        :k-2-2-3 "v-2-2-3"}}})))
+
+;;;; ___________________________________________________________________________
+;;;; ---- sut/indexed ----
+
+(fact "`sut/indexed` works"
+  (sut/indexed [:a :b :c :d])
+  => [[0 :a]
+      [1 :b]
+      [2 :c]
+      [3 :d]])
+
+;;;; ___________________________________________________________________________
+;;;; ---- sut/position ----
+;;;; ---- sut/positions ----
+
+(fact "sut/`position` and sut/`positions` work"
+  (fact "`sut/position` tests"
+    (sut/position even? []) => nil
+    (sut/position even? [12]) => 0
+    (sut/position even? [11 13 14]) => 2
+    (sut/position even? [11 13 14 14]) => 2)
+  (fact "`sut/positions` tests"
+    (sut/positions even? []) => []
+    (sut/positions even? [12]) => [0]
+    (sut/positions even? [11 13 14]) => [2]
+    (sut/positions even? [11 13 14 14 15]) => [2 3]))
+
+;;;; ___________________________________________________________________________
+;;;; ---- sut/unchunk ----
+
+(defn fun-with-return-args-to-even?-and-identity [fun]
+  (let [clj-even?     even?
+        clj-identity  identity
+        even?-acc     (atom [])
+        identity?-acc (atom [])]
+    (with-redefs [even? (fn [x]
+                          (swap! even?-acc conj x)
+                          (clj-even? x))
+                  identity (fn [x]
+                             (swap! identity?-acc conj x)
+                             x)]
+      (fun)
+      [@even?-acc
+       @identity?-acc])))
+
+(defmacro with-return-args-to-even?-and-identity
+  "Helper for testing `sut/unchunk`.
+  Execute `body` in a scope that redefines `even?` and `identity to make a note
+  of their argument. After executing `body`, return a vector whose first element
+  is a sequence of all the arguments passed to `even?` (in successive calls)
+  and whose second element is a sequence of all the arguments passed to
+  `identity` (in successive calls)."
+  [& body]
+  `(fun-with-return-args-to-even?-and-identity (fn [] ~@body)))
+
+(fact "`sut/unchunk` works"
+  (let [my-chunked-seq (range 10)]
+
+    (fact "`my-chunked-seq` is indeed chunked"
+      (fact (chunked-seq? my-chunked-seq) => true)
+      (fact (chunked-seq? (-> my-chunked-seq rest)) => true)
+      (fact (chunked-seq? (-> my-chunked-seq rest rest)) => true))
+    
+    (fact "`(sut/unchunk my-chunked-seq)` is not chunked"
+      (fact (chunked-seq? (sut/unchunk my-chunked-seq)) => false)
+      (fact (chunked-seq? (-> (sut/unchunk my-chunked-seq) rest)) => false)
+      (fact (chunked-seq? (-> (sut/unchunk my-chunked-seq) rest rest)) => false))
+
+    (fact "sut/unchunking a sequence doesn't change its value"
+      (= my-chunked-seq
+         (sut/unchunk my-chunked-seq))))
+
+  (fact "A more explicit exploration of the values that are realised"
+    (fact "Without `sut/unchunk`, `map` consumes elements we don't ultimately need"
+      (with-return-args-to-even?-and-identity
+        (every? even?
+                (map identity [2 4 6 7 8 10])))
+      =>
+      [[2 4 6 7]
+       [2 4 6 7 8 10]])
+    (fact "With `sut/unchunk`, `map` only consumes elements we ultimately need"
+      (with-return-args-to-even?-and-identity
+        (every? even?
+                (map identity
+                     (sut/unchunk [2 4 6 7 8 10]))))
+      =>
+      [[2 4 6 7]
+       [2 4 6 7]])))
+
+;;;; ___________________________________________________________________________
+;;;; ---- sut/last-index-of-char-in-string ----
+
+(fact "`sut/last-index-of-char-in-string` works"
+  (fact (sut/last-index-of-char-in-string \c "") => -1
+    (sut/last-index-of-char-in-string \c "xyz") => -1
+    (sut/last-index-of-char-in-string \c "c") => 0
+    (sut/last-index-of-char-in-string \c "abc") => 2
+    (sut/last-index-of-char-in-string \c "abcde") => 2
+    (sut/last-index-of-char-in-string \c "abcce") => 3))
