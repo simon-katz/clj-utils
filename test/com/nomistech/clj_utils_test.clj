a65e7944be461e18342cf7298e8fc65bfb51e306
Simon Katz <sk@nomistech.com>
2018-06-09 16:44:41 +0100

    Initial commit

---
+++ test/com/nomistech/clj_utils_test.clj
@@ -0,0 +1,7 @@
+(ns com.nomistech.clj-utils-test
+  (:require [clojure.test :refer :all]
+            [com.nomistech.clj-utils :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
