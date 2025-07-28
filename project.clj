9eeff045c5a69099a18a48f169c2d68f1772be62
Simon Katz <sk@nomistech.com>
2025-07-28 10:43:34 +0100

    Upgrade to Clojure 1.12.1

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/simon-katz/clj-utils"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.12.1"]]
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[compojure "1.6.1"]
                                   [nomis-clj-repl-tools "0.1.2"]
